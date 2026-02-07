6e98b90b88aca87b880fb520846846716026c403
Ayush Prakash <ayuxh26@Ayushs-MacBook-Air.local>
2026-02-07 15:26:20 +0530

    Mongo Uri Replace

--- config/database.js
+++ config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://ayuxh26:ayush260108@to-do.i95usc2.mongodb.net/tasks",
-  );
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connectDB;

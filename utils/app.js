9acfb244b08e0f2746884a6add0424ebd1ace2a9
Ayush Prakash <ayuxh26@Ayushs-MacBook-Air.local>
2026-02-18 00:44:42 +0530

    Forgot Password Functionality

--- utils/app.js
+++ utils/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const connectDB = require("../config/database");
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [process.env.FRONTEND_URL];
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5500"];
 
 app.use(
   cors({
